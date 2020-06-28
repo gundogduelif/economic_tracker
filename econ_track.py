928fb89c322d5c587ccfb3666bc588808b531493
gundogduelif <eg3068@stern.nyu.edu>
2020-06-28 19:12:49 -0400

    updates chart1 and deleted comments

--- econ_track.py
+++ econ_track.py
@@ -15,19 +15,20 @@ api_key = os.environ.get("FRED_API_KEY")
 
 # User Input and API Pull
 
-try:
-    state = input("Please input a state abbreviation: ")
-    FRED_series_id = (state) + "UR"
-    request_url = f"https://api.stlouisfed.org/fred/series/observations?series_id={FRED_series_id}&api_key={api_key}&file_type=json"
-    response = requests.get(request_url)
+while True: 
+    try:
+        state = input("Please input a state abbreviation: ")
+        FRED_series_id = (state) + "UR"
+        request_url = f"https://api.stlouisfed.org/fred/series/observations?series_id={FRED_series_id}&api_key={api_key}&file_type=json"
+        response = requests.get(request_url)
 
-    parsed_response = json.loads(response.text)
+        parsed_response = json.loads(response.text)
 
-    total_observations = parsed_response["count"]
+        total_observations = parsed_response["count"]
+        break
 
-except KeyError:
-    print("Hey, didn't find that location. Try again please.")
-    exit()
+    except KeyError:
+        print("Hey, didn't find that state. Please try again with a state abbreviation.")
 
 # Getting the state's most recent UR
 
@@ -45,6 +46,14 @@ for v in parsed_response["observations"]:
 
 all_time_high = max(all_values)
 all_time_low = min(all_values)
+
+matching_dates_low = [v for v in parsed_response["observations"] if float(v["value"]) == all_time_low]
+matching_date_low = matching_dates_low[0]
+all_time_low_date = matching_date_low["date"]
+
+matching_dates_high = [v for v in parsed_response["observations"] if float(v["value"]) == all_time_high]
+matching_date_high = matching_dates_high[0]
+all_time_high_date = matching_date_high["date"]
 
 # Getting the state's pre-COVID-19 unemployment rate
 
@@ -90,19 +99,22 @@ print("----------------------------------------------------------------------")
 import plotly.express as px
 
 all_val = []
-a = -1
+k = -1
 for w in parsed_response["observations"]:
-    a = a + 1
-    valuec = float(parsed_response["observations"][a]["value"])
-    all_val.append(valuec)
+    k += 1
+    valuec = float(parsed_response["observations"][k]["value"])
+    all_val.append(valuec) 
 values = all_val 
+
+
 all_date = []
-c = -1
+m = -1
 for y in parsed_response["observations"]:
-    c += 1
-    val_date = parsed_response["observations"][c]["date"]
+    m += 1
+    val_date = parsed_response["observations"][m]["date"]
     all_date.append(val_date)
 dates = all_date 
+
 
 fig = px.line(x=dates, y=values, title='State Unemployment Rate Time Series')
 fig.update_xaxes(
@@ -114,89 +126,52 @@ fig.update_xaxes(
             dict(count=1, label="YTD", step="year", stepmode="todate"),
             dict(count=1, label="1y", step="year", stepmode="backward"),
             dict(step="all")])))
+fig.update_yaxes(ticksuffix="%") #reference : https://plotly.com/python/axes/
 fig.update_layout(xaxis_title='Date',yaxis_title='Value %')
+
 plotly.offline.plot(fig)
 
 
 # Data Visualization 2 REFERENCE: https://plotly.com/python/line-charts/
-#  visualization problem: selected state count "count":533 < national count "count":869
+
+all_val2 = []
+a = -1
+for w in parsed_response["observations"]:
+    a += 1
+    valuec2 = float(parsed_response["observations"][a]["value"])
+    all_val2.append(valuec2)
+values2 = all_val2
+
+all_val_us2 = []
+b = -1
+for x in parsed_response["observations"]:
+    b += 1
+    valuec_us2 = float(parsed_response_us["observations"][b]["value"])
+    all_val_us2.append(valuec_us2)
+values_us2 = all_val_us2
 
 
-#SELECTED STATE https://api.stlouisfed.org/fred/series/observations?series_id=NYUR&api_key=b735cd52d104e73ee4a39522180f228b&file_type=json
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","observation_start":"1600-01-01",
-##"observation_end":"9999-12-31","units":"lin","output_type":1,"file_type":"json","order_by":"observation_date",
-##"sort_order":"asc","count":533,"offset":0,"limit":100000,"observations":
-##
-##[{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1976-01-01","value":"10.3"},
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1976-02-01","value":"10.300"},
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1976-03-01","value":"10.200"},
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1976-04-01","value":"10.200"}
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-05-01","value":"14.5"}]}
-##{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-02-01","value":"3.7"},
-# {"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-03-01","value":"4.1"},
-# {"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-04-01","value":"15.3"},
-# {"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-05-01","value":"14.5"}]}
+all_date2 = []
+c = -1
+for y in parsed_response["observations"]:
+    c += 1
+    val_date2 = parsed_response["observations"][c]["date"]
+    all_date2.append(val_date2)
+dates2 = all_date2   
 
+all_us_date2 = []
+d = -1
+for z in parsed_response_us["observations"]:
+    d += 1
+    us_date2 = parsed_response_us["observations"][d]["date"]
+    all_us_date2.append(us_date2)
+us_dates2 = all_us_date2  
 
-#US NATIONAL  https://api.stlouisfed.org/fred/series/observations?series_id=UNRATE&api_key=b735cd52d104e73ee4a39522180f228b&file_type=json
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","observation_start":"1600-01-01",
-#"observation_end":"9999-12-31","units":"lin","output_type":1,"file_type":"json","order_by":"observation_date",
-#"sort_order":"asc","count":869,"offset":0,"limit":100000,"observations":
-#[{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1948-01-01","value":"3.4"},
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1948-02-01","value":"3.8"},
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"1948-03-01","value":"4.0"}
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-03-01","value":"4.4"},
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-04-01","value":"14.7"},
-#{"realtime_start":"2020-06-25","realtime_end":"2020-06-25","date":"2020-05-01","value":"13.3"}]}
-
-
-#all_val = []
-#a = -1
-#for w in parsed_response["observations"]:
-#    a = a + 1
-#    valuec = float(parsed_response["observations"][a]["value"])
-#    all_val.append(valuec)
-#values = all_val
-#
-#all_val_us = []
-#b = -1
-#for x in parsed_response["observations"]:
-#    b += 1
-#    valuec_us = float(parsed_response_us["observations"][b]["value"])
-#    all_val_us.append(valuec_us)
-#values_us = all_val_us
-#
-#
-#all_date = []
-#c = -1
-#for y in parsed_response["observations"]:
-#    c += 1
-#    val_date = parsed_response["observations"][c]["date"]
-#    all_date.append(val_date)
-#dates = all_date    
-#
-#all_us_date = []
-#d = -1
-#for z in parsed_response_us["observations"]:
-#    d += 1
-#    us_date = parsed_response_us["observations"][d]["date"]
-#    all_us_date.append(us_date)
-#us_dates = all_us_date  
-#
-#fig = go.Figure()
-## Create and style traces
-#fig.add_trace(go.Scatter(x=all_date, y=values, name='selected state',line = dict(color='firebrick', width=4)))
-#fig.add_trace(go.Scatter(x=all_us_date, y=values_us, range_x=['1978-01-01', '2020-05-01'], name='national',line = dict(color='royalblue', width=4, dash='dash')))
-#
-## Edit the layout
-#fig.update_layout(title='Unemployment Rates for Selected State & National',
-#                   xaxis_title='Date',
-#                   yaxis_title='Value')
-#plotly.offline.plot(fig)
-#
-#
-# Alternative style traces
-#fig.add_trace(go.Scatter(x=month, y=low_2014, name = 'Low 2014',line=dict(color='royalblue', width=4)))
-#fig.add_trace(go.Scatter(x=month, y=high_2007, name='High 2007',line=dict(color='firebrick', width=4,dash='dash') # dash options include 'dash', 'dot', and 'dashdot'))
-#fig.add_trace(go.Scatter(x=month, y=high_2000, name='High 2000',line = dict(color='firebrick', width=4, dash='dot')))
-#fig.add_trace(go.Scatter(x=month, y=low_2000, name='Low 2000',line=dict(color='royalblue', width=4, dash='dot')))
+fig2 = go.Figure()
+# Create and style traces
+fig2.add_trace(go.Scatter(x=all_date2, y=values2, name='state',line = dict(color='firebrick', width=4)))
+fig2.add_trace(go.Scatter(x=all_us_date2, y=values_us2, name='national',line = dict(color='royalblue', width=4, dash='dash')))
+#range_x=['1978-01-01', '2020-05-01'], 
+# Edit the layout
+fig2.update_layout(title='Unemployment Rates for Selected State & National', xaxis_title='Date', yaxis_title='Value')
+plotly.offline.plot(fig2)
